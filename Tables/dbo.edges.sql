783d106d0bf7c23ad806f0c501f2b16163fa016b
Javed Iqbal <smile110@gmail.com>
2020-02-03 15:31:47 +0500

    Update dbo.edges.sql

--- Tables/dbo.edges.sql
+++ Tables/dbo.edges.sql
@@ -4,6 +4,6 @@ CREATE TABLE [dbo].[edges]
 [target node] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [tdoi] [float] NULL,
 [sdoi] [float] NULL,
-[my_datetime] [datetime] NULL
+[mydatetime] [datetime] NULL
 ) ON [PRIMARY]
 GO
