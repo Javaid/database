f89a58d8b2236d68eb5a7e545242b323da976abe
LAPTOP-IEO23PUP\javed <smile110@gmail.com>
2020-02-03 15:16:13 +0500

    Simple Store Procedure is created.

---
+++ Stored Procedures/dbo.helloWorld.sql
@@ -0,0 +1,17 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[helloWorld] 
+	
+AS
+BEGIN
+	SET NOCOUNT ON;
+SELECT * FROM dbo.edges;
+END
+GO
